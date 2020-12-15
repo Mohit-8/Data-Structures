c7633b03556d62e61896db2cb123bbeca0ccc250
Mohit <58105910+Mohit-8@users.noreply.github.com>
2020-12-15 23:44:05 +0530

    Create identicalTrees.java

---
+++ Java/Trees/identicalTrees.java
@@ -0,0 +1,13 @@
+boolean isIdentical(Node root1, Node root2)
+	{
+	    // Code Here
+	    if(root1==null && root2==null)
+	    return true;
+	    
+	    if(root1==null || root2==null)
+	    return false;
+	    
+	    return (root1.data==root2.data && isIdentical(root1.left,root2.left) && isIdentical(root1.right,root2.right));
+	    
+	    
+	}
